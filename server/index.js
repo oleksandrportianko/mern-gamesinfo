553ce1eb8ffb5b85a56bc31051d435c30a0d1f07
oleksandrportianko <82111498+oleksandrportianko@users.noreply.github.com>
2022-04-13 15:35:15 +0300

    based project structure

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import gamesRoute from "./routes/games.js";
+
+const app = express();
+
+app.use('/games', gamesRoute);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://portianko:7nm3Hz1ntHjCiQd8@cluster0.uaueg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
+.catch((error) => console.log(error.message));
